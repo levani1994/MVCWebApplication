a5d1ade537b4e532b1aea94e85f11c6c6f740c6d
levani_jalaghonia <levanijalaghonia@gmail.com>
2019-12-24 17:48:39 +0400

    s

--- MVCWebApplication/Models/GuestResponse.cs
+++ MVCWebApplication/Models/GuestResponse.cs
@@ -10,6 +10,6 @@ namespace MVCWebApplication.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public bool WillAtend { get; set; }
+        public bool? WillAtend { get; set; }
     }
 }
